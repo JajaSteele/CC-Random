63232bb6c295c9ea4ae8538e5028f16c61dca079
Jaja Steele <superjaja05@gmail.com>
2022-05-07 18:54:34 +0200

    Create surv.lua

---
+++ IntruderSurvey/surv.lua
@@ -0,0 +1,161 @@
+radar = peripheral.find("playerDetector")
+chatbox = peripheral.find("chatBox")
+
+args = {...}
+
+local function clear()
+    term.clear()
+    term.setCursorPos(1,1)
+end
+
+if not fs.exists("/surv.cfg") or args[1] == "cfg" then
+    while true do
+        newConfig = {}
+        clear()
+        print("Hello! Welcome to configuration util.")
+        print("Enter your Username: (This username will be ignored by the radar)")
+        newConfig["username"] = io.read()
+        clear()
+        print("Enable chat-alert? (true/false) (Will send msg to the username)")
+        enable_chat = io.read()
+        clear()
+
+        if enable_chat == "true" then
+            newConfig["enablechat"] = true
+        else
+            newConfig["enablechat"] = false
+        end
+
+        print("Enable Discord Integration? (true/false)")
+        enable_dc = io.read()
+        clear()
+
+        if enable_dc == "true" then
+            newConfig["enabledc"] = true
+        else
+            newConfig["enabledc"] = false
+        end
+
+        if newConfig["enabledc"] == true then
+            print("Please copy your discord webhook link below:")
+            newConfig["dc_hook"] = io.read()
+            clear()
+        end
+
+        print("Enter the surveying range:")
+        newConfig["range"] = tonumber(io.read())
+        clear()
+
+        print("Enter the name of this place:")
+        newConfig["basename"] = io.read()
+        clear()
+
+        print("Confirm Config? (y/n)")
+        print(table.concat(newConfig,"\n"))
+        term.write("confirm: ")
+        res1 = io.read()
+        clear()
+        if res1 == "y" then break end
+    end
+    fileconfig2 = io.open("/surv.cfg","w")
+    print("Saving Config..")
+    fileconfig2:write(textutils.serialize(newConfig))
+    print("Done!")
+    fileconfig2:close()
+end
+
+   
+fileconfig1 = io.open("/surv.cfg","r")
+if fileconfig1 ~= nil then
+    config = textutils.unserialize(fileconfig1:read("*a"))
+end
+fileconfig1:close()
+
+if config["enabledc"] == true then
+    if not fs.exists("/lib") then
+        fs.makeDir("/lib")
+    end
+    if fs.exists("/DiscordHook.lua") then
+        dc_hookapi = require("/lib/DiscordHook")
+    else
+        shell.run("wget https://raw.githubusercontent.com/Wendelstein7/DiscordHook-CC/master/DiscordHook.lua /lib/DiscordHook.lua")
+        dc_hookapi = require("/lib/DiscordHook")
+    end
+    success, hook = dc_hookapi.createWebhook(config["dc_hook"])
+    success1 = hook.send("WH Connected to Program: Survey")
+    if success1 then
+        print("Successfully connected to the Webhook!")
+    else
+        print("Error! Unable to send webhook message")
+    end
+end
+
+local function msg(t,u)
+    if config["enablechat"] == true and chatbox ~= nil then
+        chatbox.sendMessage(t,u)
+    end
+end
+
+local function msgUser(t,d,u)
+    if config["enablechat"] == true and chatbox ~= nil then
+        chatbox.sendMessageToPlayer(t,d,u)
+    end
+end
+
+local function msgOwner(t,u)
+    if config["enablechat"] == true and chatbox ~= nil then
+        chatbox.sendMessageToPlayer(t,config["username"],u)
+    end
+end
+
+local function getDiff(t1,t2)
+    difflist = {}
+    dc_difflist = {}
+    chat_difflist = {}
+    print_difflist = {}
+    if #t1 == #t2 then
+        return nil
+    else
+        if #t1 > #t2 then
+            for i1=1, #t1 do
+                if t2[i1] == nil and t1[i1] ~= config["username"] then
+                    table.insert(difflist,t1[i1])
+                    table.insert(chat_difflist,"§e§l"..t1[i1].." §aEntered §e§l"..config["basename"])
+                    table.insert(print_difflist,t1[i1].." Entered "..config["basename"])
+                    table.insert(dc_difflist,"**"..t1[i1].."** Entered **"..config["basename"].."**")
+                end
+            end
+        elseif #t1 < #t2 then
+            for i1=1, #t2 do
+                if t1[i1] == nil and t2[i1] ~= config["username"] then
+                    table.insert(difflist,t2[i1])
+                    table.insert(chat_difflist,"§e§l"..t2[i1].." §cLeft §e§l"..config["basename"])
+                    table.insert(print_difflist,t2[i1].." Left "..config["basename"])
+                    table.insert(dc_difflist,"**"..t2[i1].."** Left **"..config["basename"].."**")
+                end
+            end
+        end
+        return difflist, chat_difflist, dc_difflist, print_difflist
+    end
+end
+
+
+msgOwner("Starting Survey!")
+msgOwner("Range: "..config["range"])
+
+while true do
+    if old_ply == nil then
+        old_ply = radar.getPlayersInRange(config["range"])
+    else
+        old_ply = current_ply
+    end
+    current_ply = radar.getPlayersInRange(config["range"])
+    os.sleep(1)
+    diffs,chat_diffs,dc_diffs,print_diffs = getDiff(current_ply,old_ply)
+    
+    if diffs ~= nil then
+        hook.send(table.concat(dc_diffs,"\n"))
+        msgOwner(table.concat(chat_diffs,"\n"))
+        print(table.concat(print_diffs,"\n"))
+    end
+end

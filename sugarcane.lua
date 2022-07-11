045aa3eadca0222b3015eecf41eb9ccdb649ecf5
Jaja Steele <38012296+superjaja05@users.noreply.github.com>
2022-07-12 00:46:08 +0200

    Update sugarcane.lua

--- sugarcane.lua
+++ sugarcane.lua
@@ -12,45 +12,49 @@ while true do
     waiting = false
   end
   if turtle.inspect() then
-    print("Starting!")
-    for i1=1, 16 do
+    if turtle.getFuelLevel() >= 64 then
+      print("Starting!")
+      for i1=1, 16 do
+        mineCane()
+      end
+      print("Row n°1 done.")
+      turtle.turnRight()
       mineCane()
-    end
-    print("Row n°1 done.")
-    turtle.turnRight()
-    mineCane()
-    turtle.turnRight()
-    for i1=1, 15 do
-      mineCane()
-    end
-    print("Row n°2 done.")
-    turtle.turnLeft()
-    turtle.forward()
-    mineCane()
-    turtle.turnLeft()
-    for i1=1, 15 do
-      mineCane()
-    end
-    print("Row n°3 done.")
-    turtle.turnRight()
-    mineCane()
-    turtle.turnRight()
-    for i1=1, 15 do
-      mineCane()
-    end
-    print("Row n°4 done.\nReturning to Home.")
-    turtle.forward()
-    turtle.turnRight()
-    for i1=1, 4 do
+      turtle.turnRight()
+      for i1=1, 15 do
+        mineCane()
+      end
+      print("Row n°2 done.")
+      turtle.turnLeft()
       turtle.forward()
+      mineCane()
+      turtle.turnLeft()
+      for i1=1, 15 do
+        mineCane()
+      end
+      print("Row n°3 done.")
+      turtle.turnRight()
+      mineCane()
+      turtle.turnRight()
+      for i1=1, 15 do
+        mineCane()
+      end
+      print("Row n°4 done.\nReturning to Home.")
+      turtle.forward()
+      turtle.turnRight()
+      for i1=1, 4 do
+        turtle.forward()
+      end
+      turtle.turnRight()
+      print("Dropping All..")
+      for i1=1, 16 do
+        turtle.select(i1)
+        turtle.dropDown()
+      end
+      waiting = true
+    else
+      print("Warning! No Fuel!")
     end
-    turtle.turnRight()
-    print("Dropping All..")
-    for i1=1, 16 do
-      turtle.select(i1)
-      turtle.dropDown()
-    end
-    waiting = true
   end
   os.sleep(2)
 end

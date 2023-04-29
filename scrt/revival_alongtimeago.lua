528a6f3d18d86ca83156729ba719d627bebe7576
Jaja Steele <superjaja05@gmail.com>
2023-04-29 09:46:44 +0200

    t

--- scrt/revival_alongtimeago.lua
+++ scrt/revival_alongtimeago.lua
@@ -1,7 +1,24 @@
 local file = http.get("https://raw.githubusercontent.com/JJS-Laboratories/CC-Random/main/scrt/alongtimeago.lua")
 local cb = peripheral.find("chatBox")
 
-local filmText = "local monitor = peripheral.find('monitor')"
+local filmText = [[
+    local monitor = peripheral.find('monitor')
+    local function iterator()
+        local w,h = monitor.getSize()
+        return coroutine.wrap( function()
+            for line in string.gmatch( filmText, "([^\n]*)\n") do
+                count = count+1
+                coroutine.yield(line)
+                clearLine(h)
+                monitor.setCursorPos(2, h)
+                monitor.write("["..string.rep("#", (w-2)*(count/length)))
+                monitor.setCursorPos(w, h)
+                monitor.write("]")
+            end
+            return false
+        end )
+    end\n
+]]
 
 filmText = filmText..file.readAll()
 

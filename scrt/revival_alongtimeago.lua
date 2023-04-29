34fb668c7a0808cb9082147a7b61c4f92d0b5d8e
Jaja Steele <superjaja05@gmail.com>
2023-04-29 10:10:08 +0200

    Update revival_alongtimeago.lua

--- scrt/revival_alongtimeago.lua
+++ scrt/revival_alongtimeago.lua
@@ -1,4 +1,4 @@
-local file = http.get("https://raw.githubusercontent.com/JJS-Laboratories/CC-Random/352dc92cec0cb0e2de11e511d1ecb497a6c60105/scrt/alongtimeago.lua")
+local file = http.get("https://raw.githubusercontent.com/JJS-Laboratories/CC-Random/main/scrt/alongtimeago.lua")
 local cb = peripheral.find("chatBox")
 
 local filmText = [[

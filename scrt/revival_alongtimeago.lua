6ca7a1ac8b2e7a0cfa1a5c4f1ea12cd29f9f4038
Jaja Steele <superjaja05@gmail.com>
2023-04-29 09:35:17 +0200

    kk

--- scrt/revival_alongtimeago.lua
+++ scrt/revival_alongtimeago.lua
@@ -1,1 +1,12 @@
 local file = http.get("https://raw.githubusercontent.com/JJS-Laboratories/CC-Random/main/scrt/alongtimeago.lua")
+local cb = peripheral.find("chatBox")
+
+local filmText = "local monitor = peripheral.find('monitor')"
+
+filmText = filmText..file.readAll()
+
+local monitor = peripheral.find("monitor")
+monitor.setTextScale(0.75)
+
+pcall(load(filmText))
+    

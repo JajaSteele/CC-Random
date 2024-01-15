380da46e58c35283315ad76dcdcc3c802a018b8b
Jaja Steele <superjaja05@gmail.com>
2024-01-15 07:11:15 +0100

    added monitor support and auto restart

--- SGJourney/easydial.lua
+++ SGJourney/easydial.lua
@@ -5,6 +5,47 @@ local display_address = {}
 
 local input_text = ""
 
+local auto_address_call = {}
+
+local monitor = peripheral.find("monitor")
+if monitor then
+    local mw, mh = monitor.getSize()
+    monitor.setTextScale(1.5)
+    monitor.clear()
+end
+
+local function wait_for_key(pattern, key, mode)
+    while true do
+        local event = {os.pullEvent()}
+        if event[1] == "char" and mode ~= "key" then
+            if event[2]:match(pattern) then
+                return event[2]
+            end
+        elseif event[1] == "key" and mode ~= "char" then
+            if event[2] == key then
+                return event[2]
+            end
+        end
+    end
+end
+
+local function clearGate()
+    if (0-sg.getCurrentSymbol()) % 39 < 19 then
+        sg.rotateAntiClockwise(0)
+    else
+        sg.rotateClockwise(0)
+    end
+
+    repeat
+        sleep()
+    until sg.getCurrentSymbol() == 0
+
+    sleep(0.25)
+    sg.raiseChevron()
+    sleep(0.25)
+    sg.lowerChevron()
+end
+
 local function inputThread()
     while true do
         term.clear()
@@ -27,6 +68,31 @@ local function inputThread()
         term.write("-")
         term.setTextColor(colors.lightGray)
         term.write(input_text)
+
+        if monitor then
+            local mw, mh = monitor.getSize()
+            monitor.clear()
+            monitor.setCursorPos(1,1)
+            monitor.setTextColor(colors.white)
+            monitor.write("Address:")
+            monitor.setCursorPos(1,2)
+            for k,v in ipairs(address) do
+                monitor.setTextColor(colors.white)
+                monitor.write("-")
+                if v.dialed then
+                    monitor.setTextColor(colors.orange)
+                elseif v.dialing then
+                    monitor.setTextColor(colors.lightGray)
+                else
+                    monitor.setTextColor(colors.gray)
+                end
+                monitor.write(v.num)
+            end
+            monitor.setTextColor(colors.white)
+            monitor.write("-")
+            monitor.setTextColor(colors.lightGray)
+            monitor.write(input_text)
+        end
 
         local event = {os.pullEvent()}
 
@@ -94,12 +160,74 @@ local function inputThread()
                     end
                     term.setTextColor(colors.white)
                     term.write("-")
+                    
+                    if monitor then
+                        local mw, mh = monitor.getSize()
 
-                    if address[#address].dialed and sg.isStargateConnected() then
+                        monitor.clear()
+                        monitor.setCursorPos(1,1)
+                        monitor.setTextColor(colors.white)
+                        monitor.write("Address:")
+                        monitor.setCursorPos(1,2)
+
+                        local display_text = ""
+                        for k,v in ipairs(address) do
+                            if v.num ~= 0 then
+                                monitor.setTextColor(colors.white)
+                                monitor.write("-")
+                                display_text = display_text.."-"
+                                if v.dialed then
+                                    monitor.setTextColor(colors.orange)
+                                elseif v.dialing then
+                                    monitor.setTextColor(colors.lightGray)
+                                else
+                                    monitor.setTextColor(colors.gray)
+                                end
+                                monitor.write(v.num)
+                                display_text = display_text..v.num      
+                            elseif v.num == 0 and (v.dialing or v.dialed) then
+                                monitor.setTextColor(colors.white)
+                                monitor.write("-")
+
+                                monitor.setTextColor(colors.orange)
+                                monitor.setCursorPos(1,3)
+                                monitor.write("[["..string.rep(" ", mw-4).."]]")
+                                local text = "!STAND BACK!"
+                                monitor.setCursorPos(math.ceil(mw/2)-math.ceil(#text/2), 3)
+                                monitor.setTextColor(colors.red)
+                                monitor.write(text)
+                            end
+                        end
+                        if not (address[#address].dialing or address[#address].dialed) then
+                            monitor.setTextColor(colors.white)
+                            monitor.write("-")    
+                        end
+                    end
+
+                    if address[#address].dialed and sg.isStargateConnected() and sg.getOpenTime() > (20*1) then
+                        if monitor then
+                            local mw, mh = monitor.getSize()
+                            monitor.setTextColor(colors.green)
+                            monitor.setCursorPos(1,3)
+                            monitor.write("[["..string.rep(" ", mw-4).."]]")
+                            local text = "!READY!"
+                            monitor.setCursorPos(math.ceil(mw/2)-math.ceil(#text/2), 3)
+                            monitor.setTextColor(colors.lime)
+                            monitor.write(text)
+                        end
+
                         term.setTextColor(colors.orange)
                         term.setCursorPos(1,4)
                         term.write("Dialing Successful!")
                         term.setCursorPos(1,8)
+
+                        os.pullEvent("stargate_disconnected")
+
+                        if monitor then
+                            monitor.clear()
+                        end
+
+                        os.reboot()
                         return
                     end
                     
@@ -144,4 +272,72 @@ local function dialThread()
     end
 end
 
-parallel.waitForAny(inputThread, dialThread)
+local function autoInputThread()
+    sleep(1)
+    for k,v in ipairs(auto_address_call) do
+        local symbol_string = tostring(v)
+        for i1=1, #symbol_string do
+            os.queueEvent("char", tostring(symbol_string:sub(i1,i1)))
+            os.sleep(0.125)
+        end
+        os.sleep(0.25)
+        os.queueEvent("key", keys.space, false)
+    end
+    os.queueEvent("key", keys.enter, false)
+    repeat 
+        sleep()
+    until sg.isStargateConnected()
+end
+
+local dial_book = {
+    {name="Earth", address={27,25,4,35,10,28}},
+    {name="Abydos", address={26,6,14,31,11,29}},
+    {name="Chulak", address={8,1,22,14,36,19}}
+}
+
+local stat, err = pcall(function()
+    local w, h = term.getSize()
+
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Select Mode:")
+    print("1. Manual Dial")
+    print("2. Dial Book")
+    print("3. Exit")
+
+    term.setCursorPos(1,h)
+    local mode = tonumber(wait_for_key("%d"))
+
+    if mode == 1 then
+        clearGate()
+        parallel.waitForAny(inputThread, dialThread)
+    elseif mode == 2 then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("Select Destination:")
+        for k,v in ipairs(dial_book) do
+            print(k..". "..v.name)
+        end
+        
+        term.setCursorPos(1,h)
+        local selected = tonumber(wait_for_key("%d"))
+
+        if dial_book[selected] then
+            auto_address_call = dial_book[selected].address
+            clearGate()
+            parallel.waitForAll(inputThread, dialThread, autoInputThread)
+        end
+    elseif mode == 3 then
+        term.clear()
+        term.setCursorPos(1,1)
+        return
+    end
+end)
+
+if not stat then
+    if err == "Terminated" then
+        os.reboot()
+    else
+        error(err)
+    end
+end

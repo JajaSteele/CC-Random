d8aed5eb5d35db7a5680a0838f3fc177705c451a
Jaja Steele <superjaja05@gmail.com>
2024-10-15 12:15:20 +0200

    Logging for tempgate, fix for auto dial back

--- SGJourney/Temp Turtle Gate/server.lua
+++ SGJourney/Temp Turtle Gate/server.lua
@@ -11,7 +11,7 @@ local chat_name = "\xA76TempGate\xA7f"
 
 local function getPlayerDim(username)
     local player = player_detector.getPlayer(username)
-    return player.dimension
+    return player.dimension, player
 end
 
 local function findTurtleSlot(dim)
@@ -23,15 +23,42 @@ local function findTurtleSlot(dim)
     end
 end
 
+local function log(text)
+    local log_read = io.open("/server_log.txt", "r")
+    local logs = {}
+    if log_read then
+        for line in log_read:lines() do
+            logs[#logs+1] = line:gsub("\n", "")
+        end
+        log_read:close()
+    end
+
+    logs[#logs+1] = os.date("[%H:%M:%S] ")..text
+    if #logs > 60 then
+        table.remove(logs, 1)
+    end
+
+    local log_write = io.open("/server_log.txt", "w")
+
+    for k, line in ipairs(logs) do
+        log_write:write(line.."\n")
+    end
+    log_write:close()
+end
+
+local random_id = 0
+
 local function mainThread()
     while true do
         local event, username, message, uuid, isHidden = os.pullEvent("chat")
         if isHidden and message == "tempgate" and username == inv_manager.getOwner() then
             print("Received request..")
             if not in_use then
-                local player_dim = getPlayerDim(username)
+                local player_dim, data = getPlayerDim(username)
                 if player_dim then
-                    print("Location: "..player_dim)
+                    random_id = math.random(1000,9999)
+                    print("Location: "..player_dim.."\n("..string.format("x%d y%d z%d", data.x, data.y, data.z)..")")
+                    log(random_id..": "..player_dim.." | "..string.format("x%d y%d z%d", data.x, data.y, data.z))
                     local turtle_slot = findTurtleSlot(player_dim)
                     if turtle_slot then
                         print("Sending turtle and shulker to player!")
@@ -63,11 +90,13 @@ local function checkShulkerReturn()
                 shulker_gone = false
                 chatbox.sendMessageToPlayer("\xA7aShulker refilled and ready to use!", inv_manager.getOwner(), chat_name)
                 print("Shulker Received!")
+                log(random_id..": Shulker refilled")
             end
             local b = peripheral.wrap("back")
             if b and shulker_gone then
                 chatbox.sendMessageToPlayer("\xA7eShulker & Turtle returned..", inv_manager.getOwner(), chat_name)
                 shulker_gone = false
+                log(random_id..": Shulker + Turtle returned")
             end
         end
         sleep(1.5)

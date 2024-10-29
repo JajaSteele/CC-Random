32eb649661562f45d118db6d03dbe0c2ba27cbe7
Jaja Steele <superjaja05@gmail.com>
2024-10-29 21:10:10 +0100

    sgreset opens iris now

--- SGJourney/sgreset.lua
+++ SGJourney/sgreset.lua
@@ -4,8 +4,14 @@ local rotate_threads = {}
 
 for k,v in pairs(interfaces) do
     v.disconnectStargate()
+    print("Disconnecting gate")
+    if v.openIris then
+        v.openIris()
+        print("Opening iris")
+    end
     if v.rotateAntiClockwise then
         rotate_threads[#rotate_threads+1] = function()
+            print("Reseting rotation")
             if (0-v.getCurrentSymbol()) % 39 < 19 then
                 v.rotateAntiClockwise(0)
             else
@@ -21,5 +27,4 @@ end
 
 if #rotate_threads > 0 then
     parallel.waitForAll(table.unpack(rotate_threads))
-    print("Reset gate rotation")
 end

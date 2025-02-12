b4f7f88f46bef827a4a5cc6350b75a7075f8ad2a
Jaja Steele <superjaja05@gmail.com>
2025-02-12 22:31:45 +0100

    Adding player detection

--- AESS/aess.lua
+++ AESS/aess.lua
@@ -3,6 +3,27 @@ local player_detector = peripheral.find("playerDetector")
 
 local DEBUG = false
 
+local function deepcopy(o, seen)
+    seen = seen or {}
+    if o == nil then return nil end
+    if seen[o] then return seen[o] end
+  
+    local no
+    if type(o) == 'table' then
+      no = {}
+      seen[o] = no
+  
+      for k, v in next, o, nil do
+        no[deepcopy(k, seen)] = deepcopy(v, seen)
+      end
+      setmetatable(no, deepcopy(getmetatable(o), seen))
+    else -- number, string, boolean, etc
+      no = o
+    end
+    return no
+  end
+  
+
 local function fill(x,y,x1,y1,bg,fg,char, target)
     local curr_term = term.current()
     term.redirect(target or curr_term)
@@ -95,6 +116,7 @@ local function split(s, delimiter)
 end
 
 local config = {}
+local args = {...}
 
 local function loadConfig()
     if fs.exists(".aess_config.txt") then
@@ -109,22 +131,27 @@ local function writeConfig()
     file:close()
 end
 
-local config = {}
+if args[1] == "config" or not fs.exists("/.aess_config.txt") then
+    term.clear()
+    term.setCursorPos(1,1)
 
-local function loadConfig()
-    if fs.exists(".aess_points.txt") then
-        local file = io.open(".aess_points.txt", "r")
-        config = textutils.unserialise(file:read("*a"))
-        file:close()
-    end
+    print("Welcome to the config wizard!")
+    print("")
+    print("Detection Range:")
+
+    config.detection_range = tonumber(read()) or 100
+    
+    term.clear()
+    term.setCursorPos(1,1)
+
+    writeConfig()
 end
-local function writeConfig()
-    local file = io.open(".aess_points.txt", "w")
-    file:write(textutils.serialise(config))
-    file:close()
-end
+loadConfig()
 
 local width, height = term.getSize()
+
+local buffer_hour = {}
+local buffer_minute = {}
 
 local diffs = {
     item = {},
@@ -217,6 +244,8 @@ local function compareContentItems(old,new)
     end
 end
 
+local player_buffer = {}
+
 local function storageMonitorThread()
     while true do
         diffs.item = {}
@@ -224,14 +253,25 @@ local function storageMonitorThread()
         old_content.item = new_content.item or new_list
         new_content.item = new_list
         compareContentItems(old_content.item, new_content.item)
-        for k,v in pairs(diffs.item) do
-            print(v.type.." | "..v.amount.." "..v.name)
-        end
+
+        local last_diff = deepcopy(diffs)
+        local last_players = deepcopy(player_buffer)
+
+        buffer_minute[#buffer_minute+1] = {
+            diff=last_diff,
+            nearby_players=last_players
+        }
+
+        player_buffer = {}
+
         for i1=1, 5 do
-            term.setCursorPos(1,height)
+            term.setCursorPos(1,height-1)
             term.clearLine()
             term.write("Refreshing in "..(5-i1))
-            sleep(0.75)
+            term.setCursorPos(1,height)
+            term.clearLine()
+            term.write("Buffer: "..#buffer_minute.."/"..(60/5))
+            sleep(1)
         end
         term.setCursorPos(1,height)
         print("")
@@ -239,4 +279,46 @@ local function storageMonitorThread()
     end
 end
 
-parallel.waitForAll(storageMonitorThread)
+local function clockThread()
+    local time = os.epoch("utc")/1000
+    local next_min = time + (60-time%60)
+    while true do
+        local curr_time = os.epoch("utc")/1000
+        if curr_time >= next_min then
+            os.queueEvent("clock_min")
+            next_min = curr_time + (60-curr_time%60)
+        end
+        sleep()
+    end
+end
+
+local function storageMergerThread()
+    while true do
+        local data = {os.pullEvent()}
+        if data[1] == "clock_min" then
+            local merged_diffs = {
+                fluid={},
+                item={},
+                gas={}
+            }
+
+            for k, chunk in ipairs(buffer_minute) do
+                for diff_type, diff_data in pairs(chunk.diff) do
+                    if not merged_diffs[diff_type][]
+                end
+            end
+        end
+    end
+end
+
+local function detectorThread()
+    while true do
+        local players = player_detector.getPlayersInRange(config.detection_range)
+        for k,v in pairs(players) do
+            player_buffer[v] = true
+        end
+        sleep(0.5)
+    end
+end
+
+parallel.waitForAll(storageMonitorThread, detectorThread, storageMergerThread, clockThread)

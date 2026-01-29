ac83b41dd83f531c603c53e3c104c4237d8cae23
Jaja Steele <superjaja05@gmail.com>
2026-01-29 05:45:24 +0100

    quick tape dl

--- Computronics/tape_dl.lua
+++ Computronics/tape_dl.lua
@@ -28,9 +28,10 @@ term.clear()
 print("Enter Youtube ID:")
 local id = io.read()
 
-print("High Quality Mode? (y/n):")
-print("(will double file size, requires x2 playback speed)")
-local hq = io.read()
+print("High quality mode allows to have better song quality, but requires a tape twice the size/length of the song")
+print("High Quality Mode? (Y/N):")
+
+local hq = io.read():lower()
 if hq == "true" or hq == "y" or hq == "1" or hq == "" then
     hq = true
 else
@@ -39,19 +40,52 @@ end
 
 local tape_size = tape.getSize()
 
-local req = http.get("http://jajasteele.mooo.com:7277/?vidid="..id.."&hq="..tostring(hq))
+local req = http.get("http://jajasteele.mooo.com:7277/?vidid="..id.."&hq="..tostring(hq), nil, true)
 local req_size = tonumber(req.getResponseHeaders()["content-length"])
 
 print("Audio Size: "..bytesToString(req_size).." ("..secondsToDuration(req_size/6000)..")")
 print("Tape Size: "..bytesToString(tape_size).." ("..secondsToDuration(tape_size/6000)..")")
 
 if req_size > tape_size then
-    print("Warning! Tape is too small")
+    print("Error! Tape is too small")
+    print("Continue? (Y/N)")
+    local ctn = io.read():lower()
+    if ctn == "true" or ctn == "y" or ctn == "1" or ctn == "" then
+        print("")
+    else
+        print("Cancelled writing of tape")
+        return
+    end
 end
 
-tape.seek(-tape_size)
+tape.seek(-tape_size+5)
+
+print("Erasing tape data..")
+
+tape.write(string.rep(string.char(0), tape_size))
+
+tape.seek(-tape_size+5)
+
+print("Writing song to tape..")
 
 local wrote = tape.write(req.readAll())
 print("Wrote "..bytesToString(wrote).." ("..secondsToDuration(wrote/6000)..") of audio data.")
+
+print("Enter a label for the tape:")
+local label = io.read()
+local item_label = ""
+if hq then
+    item_label = "\xA76"..label
+else
+    item_label = "\xA77"..label
+end
+
+tape.setLabel(item_label)
+
+tape.seek(-tape_size)
+print("Tape '"..label.."' rewinded and ready to use!")
+if hq then
+    print("Don't forget to play this tape (or any tape with a gold label) at 200% speed!")
+end
 
 req.close()
